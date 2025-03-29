3778f08522e3ce7e4153dda0832c6fb897b71f58
Issac Prasad <issac.prasad@outlook.com>
2025-03-29 00:39:34 +0000

    Refactor removeElement method to improve logic and add detailed docstring

--- leetcode/two-pointers/easy/27_remove_element.py
+++ leetcode/two-pointers/easy/27_remove_element.py
@@ -10,8 +10,50 @@ import pytest
 
 
 class Solution:
+    """
+    Given an integer array `nums` and an integer `val`, this method removes all occurrences of `val`
+    in `nums` in-place.
+    The relative order of the elements may be changed. It returns the number of elements in `nums`
+    which are not equal to `val`.
+
+    Args:
+        nums (List[int]): The input list of integers.
+        val (int): The value to be removed from the list.
+
+    Returns:
+        int: The number of elements remaining in the list after removing all occurrences of `val`.
+
+    Note:
+        - The function modifies the input list `nums` in-place.
+        - The returned value represents the new length of the modified list.
+    """
+
+    # def removeElement(self, nums: List[int], val: int) -> int:
+    #     if not nums:
+    #         return 0
+    #     write_loc = 0
+    #     queue = []
+    #     count = 0
+    #     for i, n in enumerate(nums):
+    #         if n == val:
+    #             queue.append(i)
+    #         else:
+    #             count += 1
+    #             if queue:
+    #                 write_loc = queue.pop(0)
+    #                 nums[write_loc] = n
+    #                 queue.append(i)
+    #     return count
+
     def removeElement(self, nums: List[int], val: int) -> int:
-        return 0
+        if not nums:
+            return 0
+        left = 0
+        for num in nums:
+            if num != val:
+                nums[left] = num
+                left += 1
+        return left
 
 
 # @lc code=end
